231fda995760fc59dacc72c2800d81cb131814e2
Adithya N S <116829016+ADITHYA-NS@users.noreply.github.com>
2024-05-08 21:03:56 +0530

    Add files via upload

---
+++ js/newjs.js
@@ -0,0 +1,113 @@
+// Author : Ramaguru Radhakrishnan
+// Created Date : 11-Nov-2021
+// Updated Date : 30-Oct-2022
+// Description: To get the Google Scholar Publications and Citation Stats
+
+function openTab(tabName, event) {
+    var i, tabContent;
+  
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+  
+    document.querySelectorAll('button').forEach(function(tabButton) {
+      tabButton.classList.remove('active-tab');
+    });
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.classList.add('active-tab');
+  }
+  
+  function show_hide() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') ? 'block' : 'none';
+  }
+  
+  console.log("\nFeteching the details from Google Scholar");
+  
+  const faculty_1 = {
+    engine: "google_scholar_author",
+    author_id: "Xl_P9V0AAAAJ", // Dr. M. Sethumadhavan
+    hl: "en"
+  };
+  
+  const faculty_2 = {
+    engine: "google_scholar_author",
+    author_id: "nlt0DD4AAAAJ", // Dr. Chungath Srinivasan
+    hl: "en"
+  };
+  
+  const faculty_3 = {
+    engine: "google_scholar_author",
+    author_id: "K2n1nh0AAAAJ", // Dr. Lakshmy K.V
+    hl: "en"
+  };
+  
+  const faculty_4 = {
+    engine: "google_scholar_author",
+    author_id: "8AwtAWsAAAAJ", // Dr. Amritha P P
+    hl: "en"
+  };
+  
+  const faculty_5 = {
+    engine: "google_scholar_author",
+    author_id: "NHVcW84AAAAJ", // Dr. Praveen K
+    hl: "en"
+  };
+  
+  const faculty_6 = {
+    engine: "google_scholar_author",
+    author_id: "W6nvRkQAAAAJ", // Mr. Ashok Kumar Mohan
+    hl: "en"
+  };
+  
+  const faculty_7 = {
+    engine: "google_scholar_author",
+    author_id: "QdXcVjUAAAAJ", // Mr. Saurabh Shrivastava
+    hl: "en"
+  };
+  
+  const faculty_8 = {
+    engine: "google_scholar_author",
+    author_id: "-DjvKqgAAAAJ", // Mr. Ramaguru Radhakrishnan
+    hl: "en"
+  };
+  
+  const faculty_9 = {
+    engine: "google_scholar_author",
+    author_id: "jaxJad8AAAAJ", // Dr. Sangeetha Viswanathan
+    hl: "en"
+  };
+  
+  const faculty_10 = {
+    engine: "google_scholar_author",
+    author_id: "ZWxL_tkAAAAJ", // Ms. Ambili K N 
+    hl: "en"
+  };
+  
+  const search = new SerpApi.GoogleSearch("7f9f9c41b5d103050275e9c3258d9d45e4a4fcec333b24bff50bd3ddb4c08f09");
+  
+  console.log("******************************************");
+  
+  const callback = function(data) {
+    const author = data['author']['name'];
+    const publications = data['articles'].length;
+    const citations_c = data['cited_by']['table'][0]['citations']['all'];
+    const hindex_c = data['cited_by']['table'][1]['h_index']['all'];
+    const i10index_c = data['cited_by']['table'][2]['i10_index']['all'];
+    
+    console.log(author + " has " + publications + " publications ~~~ citations : " + citations_c + ", h-index : " + hindex_c + ", i10-index : " + i10index_c);
+    console.log("----------------------------------------------------------------------------------------------");
+  };
+  
+  // Fetch details for each faculty
+  search.json(faculty_1, callback);
+  search.json(faculty_2, callback);
+  search.json(faculty_3, callback);
+  search.json(faculty_4, callback);
+  search.json(faculty_5, callback);
+  search.json(faculty_6, callback);
+  search.json(faculty_7, callback);
+  search.json(faculty_9, callback);
+  search.json(faculty_10, callback);
+  
